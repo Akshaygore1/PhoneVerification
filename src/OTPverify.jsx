1dce431ac02f784a8d9dcff33e7870b43d9678de
Ashwini <ashwiniparaye9309@gmail.com>
2023-05-07 20:37:34 +0530

    done

--- src/OTPverify.jsx
+++ src/OTPverify.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
 import "./styles/OTP.css";
+import { useLocation } from "react-router-dom";
+import img from "./assets/img2.png";
+import toast, { Toaster } from "react-hot-toast";
 
 const OTPVerify = () => {
   const [otp, setOtp] = useState(new Array(6).fill(""));
-
-  // const location = useLocation();
-  // const OTP = location.state.OTP;
+  const location = useLocation();
 
   const handleChange = (element, index) => {
     if (isNaN(element.value)) return false;
@@ -40,49 +40,85 @@ const OTPVerify = () => {
     }
   };
 
-  // console.log("zzzzzzzzz", OTP);
+  const handlePaste = (e) => {
+    e.preventDefault();
+    const pastedData = e.clipboardData.getData("Text");
+    const otpChars = pastedData
+      .slice(0, 6)
+      .split("")
+      .filter((char) => /[0-9]/.test(char)); // Only keep numeric characters
+    otpChars.forEach((char, index) => {
+      if (index < otp.length) {
+        setOtp((prevValues) => {
+          const newValues = [...prevValues];
+          newValues[index] = char;
+          return newValues;
+        });
+        if (index < otpChars.length - 1) {
+          e.target.dispatchEvent(
+            new KeyboardEvent("keydown", { key: "Enter" })
+          );
+        }
+      }
+    });
+  };
 
+  console.log("location ==", location);
   return (
     <>
       <div className="row">
-        {/* <h1>{OTP}</h1> */}
         <div className="wrapper">
+          <img className="imgc" src={img} alt="" />
           <h2>Verify your identity</h2>
-          <p>OTP has been sent to mobile number 889933XXXX</p>
-
-          {otp.map((data, index) => {
-            return (
-              <input
-                className="otp-field"
-                type="password"
-                name="otp"
-                maxLength="1"
-                key={index}
-                value={data}
-                onChange={(e) => handleChange(e.target, index)}
-                onKeyDown={(e) => handleKeyDown(e, index)}
-                onFocus={(e) => e.target.select()}
-              />
-            );
-          })}
-
-          <p>OTP Entered - {otp.join("")}</p>
           <p>
-            <button
-              className="btn btn-secondary mr-2"
-              onClick={(e) => setOtp([...otp.map((v) => "")])}
-            >
-              Clear
-            </button>
-            <button
-              className="btn btn-primary"
-              onClick={(e) => alert("Entered OTP is " + otp.join(""))}
-            >
-              Verify OTP
-            </button>
+            OTP has been sent to mobile number
+            <span> {location.state} </span>
           </p>
+          <div className="otp-input">
+            {otp.map((data, index) => {
+              return (
+                <input
+                  className="otp-field"
+                  type="password"
+                  name="otp"
+                  maxLength="1"
+                  key={index}
+                  value={data}
+                  onChange={(e) => handleChange(e.target, index)}
+                  onKeyDown={(e) => handleKeyDown(e, index)}
+                  onPaste={index === 0 ? handlePaste : undefined}
+                  onFocus={(e) => e.target.select()}
+                />
+              );
+            })}
+          </div>
+          <div className="btn-wrapper">
+            <p>
+              <button
+                className="btn btn-secondary mr-2"
+                onClick={(e) => setOtp([...otp.map((v) => "")])}
+              >
+                Clear
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={(e) => {
+                  if (otp.includes("")) {
+                    toast.error("Please enter all OTP digits!");
+                  } else {
+                    toast.success(
+                      "Your Mobile Number has been successfully verified!"
+                    );
+                  }
+                }}
+              >
+                Verify OTP
+              </button>
+            </p>
+          </div>
         </div>
       </div>
+      <Toaster position="top-right" />
     </>
   );
 };
--- src/PhoneVerify.jsx
+++ src/PhoneVerify.jsx
@@ -2,30 +2,12 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./styles/Phone.css";
 import img from "./assets/bg2.png";
+import toast, { Toaster } from "react-hot-toast";
 
 const PhoneVerify = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
-  const [OTP, setOTP] = useState("");
-  const [showOTP, setShowOTP] = useState(false);
 
-  // const navigate = useNavigate();
-
-  const handleGenerateOTP = () => {
-    const digits = "0123456789";
-    let OTP = "";
-    for (let i = 0; i < 6; i++) {
-      OTP += digits[Math.floor(Math.random() * 10)];
-    }
-
-    setOTP(OTP);
-    setShowOTP(true);
-
-    setTimeout(() => {
-      setShowOTP(false);
-    }, 10000);
-
-    // navigate("/verify", { state: OTP });
-  };
+  const navigate = useNavigate();
 
   const handlePhoneNumberChange = (event) => {
     setPhoneNumber(event.target.value.replace(/\D/g, ""));
@@ -35,62 +17,45 @@ const PhoneVerify = () => {
 
   const handleVerifyClick = () => {
     if (isPhoneNumberValid) {
-      handleGenerateOTP();
       setPhoneNumber("");
+      navigate("/verify", { state: phoneNumber });
     } else {
-      alert("Please enter a valid 10-digit phone number");
+      toast.error("Please enter a valid 10-digit phone number");
     }
   };
 
-  // console.log("otp ===", OTP);
+  // console.log("otp ===", phoneNumber);
 
   return (
     <>
-      <div
-        className="container"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          marginTop: "10px",
-          marginBottom: "10px",
-        }}
-      >
+      <div className="container">
         <img className="img" src={img} alt="" />
-        <h1>Verify Your Phone Number</h1>
-
-        <label htmlFor="phone-number-input">Enter your phone number : </label>
+        <h1>OTP Verification</h1>
+        <p>
+          We will send you a one time password on this mobile number <br />{" "}
+          <span>{phoneNumber}</span>
+        </p>
+        <label htmlFor="phone-number-input" className="label">
+          Please enter your mobile number :{" "}
+        </label>
         <br />
-        <input
-          type="tel"
-          className="input"
-          id="phone-number-input"
-          name="phone-number"
-          value={phoneNumber}
-          onChange={handlePhoneNumberChange}
-        />
-        {phoneNumber && !isPhoneNumberValid && (
-          <p style={{ color: "red", fontWeight: "800px" }}>
-            Please enter a valid 10-digit phone number
-          </p>
-        )}
-        <br />
+        <div className="input-wrap">
+          <input
+            type="tel"
+            className="input"
+            id="phone-number-input"
+            name="phone-number"
+            value={phoneNumber}
+            onChange={handlePhoneNumberChange}
+            placeholder="+91-1234567890"
+          />
 
-        <button onClick={handleVerifyClick} style={{ width: "100px" }}>
-          Get OTP
-        </button>
-        {showOTP && <h1>{OTP}</h1>}
-        {showOTP && (
-          <button
-            onClick={() =>
-              navigator.clipboard.writeText(alert("Copied to clipboard!", OTP))
-            }
-          >
-            Copy
+          <button onClick={handleVerifyClick} style={{ width: "100px" }}>
+            Get OTP
           </button>
-        )}
+        </div>
       </div>
+      <Toaster position="top-right" />
     </>
   );
 };
